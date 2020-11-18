cd37405859c1a2328a6058beb4feda93bc7a7a6a
mikhail.lazko <mikhail.lazko@quantumsoft.ru>
2020-11-19 02:24:28 +0700

    Update annotation for GlobalExceptionHandler that generate swagger properly

--- src/main/java/com/lazko/board/widget/application/error/GlobalExceptionHandler.java
+++ src/main/java/com/lazko/board/widget/application/error/GlobalExceptionHandler.java
@@ -8,15 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
 
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
@@ -34,16 +35,18 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler({NotFoundEntityException.class})
-    protected ResponseEntity<Object> handleNotFoundEntity(NotFoundEntityException ex, WebRequest request) {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    protected ResponseEntity<ApiError> handleNotFoundEntity(NotFoundEntityException ex, WebRequest request) {
         var entityId = ex.getEntityId();
         var message = String.format("Entity with id=%s not found", entityId);
-        ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, message, entityId.toString());
-        return new ResponseEntity(apiError, new HttpHeaders(), HttpStatus.NOT_FOUND);
+        var apiError = new ApiError(HttpStatus.NOT_FOUND, message, entityId.toString());
+        return new ResponseEntity<>(apiError, new HttpHeaders(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({InvalidArgValueException.class})
-    protected ResponseEntity<Object> handleInvalidArgValue(InvalidArgValueException ex, WebRequest request) {
-        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), ex.getArgumentName());
-        return new ResponseEntity(apiError, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ResponseEntity<ApiError> handleInvalidArgValue(InvalidArgValueException ex, WebRequest request) {
+        var apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), ex.getArgumentName());
+        return new ResponseEntity<>(apiError, new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 }
